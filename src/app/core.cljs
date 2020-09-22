612cd239fe4e37536b8d9f833a01620c1338cf61
filip <fbielejec@gmail.com>
2020-09-22 15:16:58 +0200

    init repo

---
+++ src/app/core.cljs
@@ -0,0 +1,33 @@
+(ns app.core
+  (:require [clara.rules :as clara :refer-macros [defrule defsession defquery]]))
+
+(defsession session 'app.core
+  :fact-type-fn :type)
+
+(defn test-rules []
+  (-> session
+      (clara/insert {:type :temp :degrees 15})
+      (clara/fire-rules)))
+
+(defn fu []
+  (prn "bar"))
+
+(defrule is-cold-and-windy-map
+  [:temp [{degrees :degrees}] (< degrees 20) (== ?t degrees)]
+
+  =>
+
+  (prn "@@@ notifs/is-cold-and-windy-map")
+  #_(fu))
+
+(defn start! []
+  (prn "stop"))
+
+(defn stop! []
+  (prn "stop"))
+
+(defn main []
+  (start!))
+
+(comment
+  (test-rules))
